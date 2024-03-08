0a45f6e7456b09d67397c60fbf3b04dd70ceb982
Myriad-Dreamin <camiyoru@gmail.com>
2024-03-08 13:54:05 +0800

    Expose function details

--- crates/typst-library/src/foundations/func.rs
+++ crates/typst-library/src/foundations/func.rs
@@ -147,7 +147,7 @@ pub struct Func {
 
 /// The different kinds of function representations.
 #[derive(Clone, PartialEq, Hash)]
-enum FuncInner {
+pub enum FuncInner {
     /// A native Rust function.
     Native(Static<NativeFuncData>),
     /// A function for an element.
@@ -375,6 +375,11 @@ impl Func {
             self.span = span;
         }
         self
+    }
+
+    /// The function's inner.
+    pub fn inner(&self) -> &FuncInner {
+        &self.inner
     }
 }
 
